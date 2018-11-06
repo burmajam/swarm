2e69f2a3396ec1fc1aa97fc5227e4ee8b663fbf2
Milan Burmaja <burmajam@gmail.com>
2018-11-06 11:35:32 +0100

    Test that start reason is injected when incoking processes MFA

--- test/support/example_sup.ex
+++ test/support/example_sup.ex
@@ -16,4 +16,13 @@ defmodule MyApp.WorkerSup do
   def register() do
     {:ok, _pid} = Supervisor.start_child(__MODULE__, [])
   end
+
+  def register(:failover, pid) do
+    send pid, {:register_worker, :failover}
+    :no_failover_needed
+  end
+  def register(mode, pid) do
+    send pid, {:register_worker, mode}
+    {:ok, _pid} = Supervisor.start_child(__MODULE__, [])
+  end
 end
